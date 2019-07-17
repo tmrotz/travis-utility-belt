feff3ddf88dece8c699f03272018302bf6e761a6
Travis Rotz <tmrotz@gmail.com>
2019-07-17 14:56:35 -0500

    Finish downloads rough draft. Most functionality is in place

--- src/components/background/downloads.js
+++ src/components/background/downloads.js
@@ -1,7 +1,14 @@
+import Icon128 from '../../assets/icon128.png';
+import SuccessImg from '../../assets/success.png';
+import ErrorImg from '../../assets/error.png';
+const down_dict = {};
+
 
 /**
  * @param [DownloadItem]{@link https://developers.chrome.com/extensions/downloads#type-DownloadItem}
  *   dl_item - A DownloadItem object
+ * @param [function]{@link https://developer.chrome.com/extensions/downloads#event-onDeterminingFilename}
+ *   suggest - A function?
  */
 // export default async function handleDeterminingFilename(dl_item, suggest) {
 export default async function handleOnCreated(dl_item) {
@@ -27,15 +34,70 @@ export default async function handleOnCreated(dl_item) {
 
   for (let found_dl_item of found_dl_items) {
     if (dl_item.id === found_dl_item.id) {
+      console.log('found myself');
       continue;
     }
 
-    if (dl_item.state !== 'interrupted') {
-      chrome.downloads.cancel(dl_item.id);
+    if (found_dl_item.state === 'in_progress') {
+      await chrome.downloads.cancel(dl_item.id);
       console.log('cancel download: success');
-      return;
+      break;
+
+    } else if (found_dl_item.state === 'complete') {
+      console.log(found_dl_item);
+      chrome.notifications.create({
+        type: 'basic',
+        iconUrl: Icon128,
+        title: 'File already exists',
+        message: 'It seems like the file ' + dl_item.filename + ' already exists',
+        contextMessage: 'Click to show the file',
+        isClickable: true,
+        buttons: [
+          {title: 'Resume', iconUrl: SuccessImg},
+          {title: 'Cancel', iconUrl: ErrorImg}
+        ]
+      }, function (notification_id) {
+        down_dict[notification_id] = dl_item.id;
+        console.log(`Notification id: ${notification_id}`);
+      });
+      break;
     }
   }
 
-  chrome.downloads.resume(dl_item.id);
 }
+
+chrome.notifications.onButtonClicked.addListener(async (noti_id, button_index) => {
+
+  console.log(noti_id);
+  console.log('button clicked');
+  const id = down_dict[noti_id];
+  if (!id) {
+    console.log('dl-item not there?');
+    return;
+  }
+  delete down_dict[noti_id];
+
+  if (button_index === 0) {
+    console.log('Resume Download');
+    await chrome.downloads.resume(id);
+    console.log('resume download: success');
+
+  } else if (button_index === 1) {
+    console.log('Cancel download');
+    await chrome.downloads.cancel(id);
+    console.log('cancel download: success');
+  }
+
+  chrome.notifications.clear(noti_id);
+});
+
+// chrome.notifications.onClicked.addListener(function (notificationId) {
+//   if (itemsByNotificationId[notificationId]) {
+//     chrome.notifications.clear(notificationId);
+//     chrome.downloads.cancel(itemsByNotificationId[notificationId].downloadItem.id);
+//     chrome.downloads.erase({id: itemsByNotificationId[notificationId].downloadItem.id});
+//     chrome.downloads.show(itemsByNotificationId[notificationId].foundItem.id);
+//     delete itemsByExistingId[itemsByNotificationId[notificationId].foundItem.id];
+//     delete itemsByNotificationId[notificationId];
+//   }
+// });
