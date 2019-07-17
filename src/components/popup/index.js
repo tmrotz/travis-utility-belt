432f913c01d9d792df25a1f17cc0de17c2af0296
Travis Rotz <tmrotz@gmail.com>
2019-07-17 14:51:21 -0500

    Finish popup UI/logic

--- src/components/popup/index.js
+++ src/components/popup/index.js
@@ -2,17 +2,27 @@ import './styles.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import { notifySuccess } from '../../common/notifications.js';
 
-chrome.storage.local.get(['paste-text'], result => {
-  if (result['paste-text']) {
-    $('#paste-text-input').placeholder = result['paste-text'];
-  }
-});
+window.onload = function() {
+  console.log('onload');
 
-$('#paste-text-button').click(event => {
-  const paste_text = $('#paste-text-input').value;
 
-  chrome.storage.local.set({ 'paste-text': paste_text }, () => {
-    $('#paste-text-input').placeholder = paste_text;
-    notifySuccess('Paste text successfully updated!');
+  chrome.storage.local.get(['paste-text'], result => {
+    if (result['paste-text']) {
+      document.getElementById('paste-text-input').placeholder = result['paste-text'];
+    }
   });
+
+  document.getElementById('paste-text-button').onclick = () => {
+    const paste_text = document.getElementById('paste-text-input').value;
+
+    chrome.storage.local.set({ 'paste-text': paste_text }, () => {
+      document.getElementById('paste-text-input').value = '';
+      document.getElementById('paste-text-input').placeholder = paste_text;
+      notifySuccess('Paste text successfully updated!');
+    });
+  }
+};
+
+window.addEventListener('unload', function() {
+  console.log('unload');
 });
